44b0b0ae877fa9f9d814e1517a4cf5f1c531fd28
jeffery <qianjhv@gmail.com>
2024-12-16 22:23:27 +0800

    fix last commit test

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,16 @@
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 import { fetchCardData } from '@/app/lib/data';
 
+import { RevenueChartSkeleton, CardsSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
-
 import { lusitana } from '@/app/ui/fonts';
  
+export const experimental_ppr = true;
+
 export default async function Page() {
 
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers  } = await fetchCardData();
@@ -28,7 +29,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
